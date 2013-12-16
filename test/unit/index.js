f0cf551451dbd98f6e28572874adfcf55836cd3a
Kenneth Lee <kennethkl@gmail.com>
2013-12-16 12:21:13 -0500

    wip: refactored cli to export commands

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,19 +1,39 @@
-var mocha = require('mocha'),
+var fs = require('fs'),
+    exec = require('child_process').exec,
+    assert = require('assert'),
     mongoose = require('mongoose'),
-    migrate = require('../');
+    cli = require('../../lib/cli');
 
 describe('Unit:', function() {
     describe('Migrate CLI', function() {
-        it.skip('should initialize migrations', function(done) {
+
+        before(function(done) {
+            // TODO symblink bin file
+            // TODO initialize migrations
+            
+            done();
+        });
+
+        after(function(done) {
+            // TODO destroy migrations
+            // TODO unlink bin file
+            done();
         });
 
         it.skip('should create a migration script', function(done) {
+            cli.create('table', 'test cli create');
+
+            // TODO Check for file ./migrations/table-0001-test-cli-create.js
+            assert.ok(fs.existsSync('./migrations/table-0001-test-cli-create.js'));
         });
 
         it.skip('should migrate to latest', function(done) {
         });
 
         it.skip('should rollback', function(done) {
+        });
+
+        it.skip('should destroy all migrations', function(done) {
         });
     });
 });
